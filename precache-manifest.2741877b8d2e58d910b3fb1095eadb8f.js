0a8896f84d1f39b0e1284865837a06ebd0ee7f6a
wlshen@umich.edu <wlshen@umich.edu>
2019-08-09 03:01:06 -0400

    Updates

---
+++ precache-manifest.2741877b8d2e58d910b3fb1095eadb8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabf8467fa99837d6db391e949ff57de",
+    "url": "/minimize/index.html"
+  },
+  {
+    "revision": "5f1088b658f49a4f0f53",
+    "url": "/minimize/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/minimize/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "5f1088b658f49a4f0f53",
+    "url": "/minimize/static/js/main.b26da7e1.chunk.js"
+  },
+  {
+    "revision": "541c763685673c86a8fd",
+    "url": "/minimize/static/js/runtime~main.3771e673.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/minimize/static/media/logo.5d5d9eef.svg"
+  }
+]);
